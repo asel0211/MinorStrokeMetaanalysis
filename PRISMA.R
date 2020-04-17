148da16cb09870e70eec00aaf6679d4c3b7794fd
asel0211 <61904820+asel0211@users.noreply.github.com>
2020-04-17 22:25:09 +1000

    Create PRISMA.R

---
+++ PRISMA.R
@@ -0,0 +1,12 @@
+library(PRISMAstatement)
+# Adapted from gntem2. Example from Spot sign paper. Stroke 2019
+prisma(found = 193,
+       found_other = 27,
+       no_dupes = 141, 
+       screened = 141, 
+       screen_exclusions = 3, 
+       full_text = 138,
+       full_text_exclusions = 112, 
+       qualitative = 26, 
+       quantitative = 26,
+       width = 800, height = 800)
